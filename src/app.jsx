844a119255e65e28711da8365368823028ab59ca
lubshad <lubshad4u4@gmail.com>
2021-12-19 21:00:12 +0530

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, doloremque voluptatum aperiam, necessitatibus illum ea maxime fugit eveniet amet consectetur optio accusamus molestias, labore eos ratione totam! Molestiae, suscipit ea
+    </div>
+  );
+}
+
+export default App;
