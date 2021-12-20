02212e09c9da3bf683536b5df26f64d09c7f67d7
lubshad <lubshad4u4@gmail.com>
2021-12-20 18:24:30 +0530

    hover video player added

--- src/home/components/movie_carousal/components/MovieCarousalItem.jsx
+++ src/home/components/movie_carousal/components/MovieCarousalItem.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import "./movieCarousalItem.scss"
 import { Add, ArrowLeft, ArrowRight, ThumbDown, ThumbUp, PlayArrow } from "@mui/icons-material"
+import { useState } from 'react';
 
 
-export default function MovieCarousalItem({index}) {
+export default function MovieCarousalItem({ index }) {
 
-    
-    const left = (index*320)-50
+    const [isHovered, setIsHovered] = useState(false);
+
+
+    const left = (index * 320) - 50
+
+    const trailer =
+        "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
 
     return (
-        <div className="carousal-item">
+        <div className="carousal-item"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg" alt="" className="carousal-item-image" />
             <div className="highlighted-carousal-item" style={{ left: left }}>
-                <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg" alt="" className="heighlighted-item-image" />
+                {isHovered && (
+                    <>
+                    <video controls className="heighlighted-item-image" src={trailer} autoPlay={true} loop />
+                    </>
+                )}
                 <div className="heighlighted-item-action-buttons">
                     <PlayArrow className="heighlighted-item-action-button"></PlayArrow>
                     <Add className="heighlighted-item-action-button"></Add>
